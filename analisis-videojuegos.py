d87af0dcc8c4928e7bb9d399100099086c453eba
Erick <erick.naunay@tripleten-team.com>
2025-06-25 21:49:48 -0500

    Agregue el grafico de numero de videojuegos lanzados por anio

--- analisis-videojuegos.py
+++ analisis-videojuegos.py
@@ -13,7 +13,11 @@ print()
 
 ventas_x_anio = juegos.groupby('Year_of_Release')['total_ventas'].sum().sort_index()
 
-ventas_x_anio.plot()
+ventas_x_anio.plot(title='Total de ventas anuales por videojuegos')
+plt.show()
+
+videojuegos_x_anio = juegos.groupby('Year_of_Release')['Name'].count().sort_index()
+videojuegos_x_anio.plot(title='Cantidad de videojuegos lanzados por añio')
 plt.show()
 
 
